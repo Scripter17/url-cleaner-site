1c9c26734c7e5e957839d52c83e2e5020f9cdf1d
James C. Wise <jameschristopherwise@gmail.com>
2024-04-23 04:24:43 -0400

    Better typing and stuff

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
 use url::Url;
 use serde::{Serialize, Deserialize};
+use std::net::{IpAddr, Ipv4Addr};
 
 #[launch]
 fn rocket() -> _ {
     rocket::custom(rocket::Config {
         port: 9149,
+        address: IpAddr::V4(Ipv4Addr::new(0,0,0,0)),
         ..rocket::Config::default()
     }).mount("/clean", routes![clean])
         .attach(Anarcors)
@@ -24,16 +26,16 @@ struct Job {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct JobResponse {
-    urls: Vec<Url>
+    urls: Vec<Result<Url, String>>
 }
 
 #[post("/", data="<job>")]
-fn clean(job: Json<Job>) -> Result<Json<JobResponse>, String> {
-    let mut job = job.0;
-    for url in job.urls.iter_mut() {
-        url_cleaner::clean_url(url, None, job.params_diff.as_ref()).map_err(|e| e.to_string())?;
-    }
-    Ok(Json(JobResponse {urls: job.urls}))
+fn clean(job: Json<Job>) -> Json<JobResponse> {
+    let job = job.0;
+    Json(JobResponse {
+        urls: job.urls.into_iter()
+            .map(|mut url| {url_cleaner::clean_url(&mut url, None, job.params_diff.as_ref()).map_err(|e| e.to_string())?; Ok(url)}).collect()
+    })
 }
 
 struct Anarcors;
--- userscript.js
+++ userscript.js
@@ -2,8 +2,8 @@
 // @name         URL Cleaner
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-16
-// @description  Quick, dirty, and shitty URL Cleaner userscript. Definitely has problems with shortlinks.
-// @author       You
+// @description  Quick and dirty URL Cleaner userscript.
+// @author       Scripter17@Github.com
 // @match        https://*/*
 // @match        http://*/*
 // @grant        GM_xmlhttpRequest
@@ -12,16 +12,23 @@
 
 async function doit() {
 	let elements = [...document.getElementsByTagName("a")]
-		.filter(e => e.href.startsWith("http") && e.getAttribute("url-cleaned") != "true");
+		.filter(e => e.href.startsWith("http") && e.getAttribute("url-cleaned") == null);
 	if (elements.length > 0) {
 		await GM_xmlhttpRequest({
 			url: "http://localhost:9149/clean",
 			method: "POST",
 			data: JSON.stringify({urls: elements.map(x => x.href)}),
 			onload: function(response) {
-				JSON.parse(response.responseText).urls.forEach(function (cleaned_url, index) {
-					elements[index].href = cleaned_url;
-					elements[index].setAttribute("url-cleaned", "true");
+				JSON.parse(response.responseText).urls.forEach(function (cleaning_result, index) {
+					if (cleaning_result.Err == null) {
+						if (elements[index].href != cleaning_result.Ok) {elements[index].href = cleaning_result.Ok;}
+						elements[index].setAttribute("url-cleaned", "true");
+					} else {
+						console.log("URL Cleaner error": cleaning_result, index, cleaning_result);
+						elements[index].setAttribute("url-cleaned", "error");
+						elements[index].setAttribute("url-cleaner-error", cleaning_result.Err);
+						elements[index].style.color = "red";
+					}
 				})
 			}
 		});
